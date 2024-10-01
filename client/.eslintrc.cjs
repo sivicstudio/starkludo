28d92b24636dbc83a389cb77f73fc43bbe0c14b3
Stephanie Nwankwo <stephybelle208@gmail.com>
2024-10-01 16:41:44 +0100

    fix: lint

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -11,5 +11,6 @@ module.exports = {
     rules: {
         "react-refresh/only-export-components": "warn",
         "@typescript-eslint/no-inferrable-types": "off",
+        "@typescript-eslint/no-empty-function": false
     },
 };
