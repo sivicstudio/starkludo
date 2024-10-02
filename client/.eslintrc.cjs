e1e334a10a3e9b4c1ccc4a0d4c4e53a54d04f1b5
Stephanie Nwankwo <stephybelle208@gmail.com>
2024-10-02 15:29:51 +0100

    fix: commitlint workflow

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -10,5 +10,12 @@ module.exports = {
     plugins: ["react-refresh"],
     rules: {
         "react-refresh/only-export-components": "warn",
+        "@typescript-eslint/no-inferrable-types": "off",
+        "no-mixed-spaces-and-tabs": 0, 
+        'prefer-const': 'warn',
+        '@typescript-eslint/no-empty-function': 'off',
+        "@typescript-eslint/ban-types": "off",
+        "@typescript-eslint/no-unused-vars": "warn",
+        "@typescript-eslint/no-explicit-any": "warn"
     },
 };
