41f554a10b0a9b2130967a563a9e97cb2b3c02b8
jedstroke <jedstroke@gmail.com>
2024-10-05 11:05:05 +0100

    changes

--- client/src/types.d.ts
+++ client/src/types.d.ts
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-export type OptionsProps = {
-  gameIsOngoing: boolean; // true if game is ongoing, false otherwise
-  playersLength: number; // Number of players in current game
-  diceFace: number; // Current number on dice face
-  playerChance: number; // Next player to take action. 0 => red, 1 blue...
-  hasThrownDice: boolean; // true if dice has been thrown and waiting for action from player, false otherwise
-  winners: number[];
-  gameCondition: number[];
-};
-
-
-type WinnerList = number[];
-
-export type GameOptions = {
-  isChance: boolean;
-  isThrown: boolean;
-  chance: number;
-  playersLength: number;
-  winners: WinnerList;
-  win: boolean;
-};
-
-//Marker Component
-export type MarkerProps = {
-  pos: string;
-  size: number;
-  tileMap: Record<number, number[]>;
-}
-=======
 export type OptionsProps = {
   gameIsOngoing: boolean; // true if game is ongoing, false otherwise
   playersLength: number; // Number of players in current game
@@ -55,4 +25,3 @@ export type MarkerProps = {
   size: number;
   tileMap: Record<number, number[]>;
 };
->>>>>>> c43c6f1d481754a137db14df3d2d25dbafa13db0
