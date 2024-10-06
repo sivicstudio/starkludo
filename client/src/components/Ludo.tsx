008ba80b1e45486ca8695dfedd131b06a7ff6bb7
martin machiebe <martin machiebe>
2024-10-06 17:46:46 +0100

    feat: add image of player profiles

--- client/src/components/Ludo.tsx
+++ client/src/components/Ludo.tsx
@@ -14,6 +14,34 @@ const Ludo: React.FC = () => {
   const { options } = useContext(GameContext);
   const { board } = useContext(BoardContext);
 
+  // Player data with correct order: red, green, yellow, blue
+  const players = [
+    {
+      id: 0,
+      name: "Player 1",
+      color: "red",
+      image: "/assets/images/profile1.jpg",
+    },
+    {
+      id: 1,
+      name: "Player 2",
+      color: "green",
+      image: "/assets/images/profile2.jpg",
+    },
+    {
+      id: 2,
+      name: "Player 3",
+      color: "yellow",
+      image: "assets/images/profile3.jpg",
+    },
+    {
+      id: 3,
+      name: "Player 4",
+      color: "blue",
+      image: "assets/images/profile4.jpg",
+    },
+  ].slice(0, options.playersLength);
+
   return (
     <div className={`container ${board} card`} ref={boardRef}>
       {options.gameIsOngoing &&
@@ -21,6 +49,23 @@ const Ludo: React.FC = () => {
           .slice(0, options.playersLength * 4)
           .map((m) => <Marker key={m} pos={m} size={size} tileMap={tileMap} />)}
       <LudoTiles />
+
+      {/* Player profile images */}
+      <div className="player-profiles">
+        {players.map((player) => (
+          <div
+            key={player.id}
+            className={`player-profile player-${player.color}`}
+          >
+            <img src={player.image} alt={player.name} />
+            <small className="username-player">{player.name}</small>
+            {options.playerChance === player.id && (
+              <div className="turn-indicator">Your Turn</div>
+            )}
+          </div>
+        ))}
+      </div>
+
       {!options.playersLength && (
         <div className="overlay">
           <div className="overlay-child">
