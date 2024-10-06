b68af03b8df09119b256a2eddd361782af3bfc8c
Jed <86930056+jedstroke@users.noreply.github.com>
2024-10-06 15:18:17 +0000

    feat: implement the action section

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,7 +10,7 @@ import Alert from "./components/Alert";
 import Footer from "./components/Footer";
 import { chance } from "./hooks/utils";
 import "react-simple-flex-grid/lib/main.css";
-import RestartGame from "./components/RestartGame";
+// import RestartGame from "./components/RestartGame";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { OptionsProps } from "./types";
@@ -19,7 +19,6 @@ import Control from "./components/Control";
 import { StarknetProvider } from "./starknet-provider";
 import { FiAlertTriangle, FiZap } from "react-icons/fi";
 import { BoardContext, BoardType } from "./context/board-context";
-import { DiceProvider } from "./context/dice-context";
 import ControlWindowLayout from "./components/ControlWindows/ControlWindowLayout";
 import GameHelp from "./components/ControlWindows/GameHelp";
 import Toolbox from "./components/ControlWindows/Toolbox";
@@ -31,7 +30,6 @@ const App = () => {
   const [activeWindow, setActiveWindow] = useState("");
   const [board, setBoard] = useState<BoardType>("classic");
   const [gameState, setGameState] = useState({});
-  const [activeCategory, setActiveCategory] = useState("BOARD");
   const [options, setOptions] = useState<OptionsProps>({
     gameIsOngoing: false,
     playersLength: 0,
@@ -52,10 +50,6 @@ const App = () => {
 
   const toggleBoard = (newBoard: BoardType) => {
     setBoard(newBoard);
-  };
-
-  const handleCategoryClick = (category: string) => {
-    setActiveCategory(category);
   };
 
   const setGameData = useCallback((game: object) => {
@@ -98,92 +92,87 @@ const App = () => {
           }}
         >
           <BoardContext.Provider value={{ board, toggleBoard }}>
-            <DiceProvider>
-              <div className="game-behaviour-warning">
-                <FiAlertTriangle size={40} style={{ marginRight: "10px" }} />
-                StarkLudo is still in active development{" "}
-                <FiZap color="yellow" size={20} />
+            <div className="game-behaviour-warning">
+              <FiAlertTriangle size={20} />
+              StarkLudo is still in active development{" "}
+              <FiZap color="yellow" size={20} />
+            </div>
+            <div className="layout-container">
+              <div className="mobile-header">
+                <Header />
               </div>
-              <div className="layout-container">
-                <div className="mobile-header">
-                  <Header />
-                </div>
-                <Row gutter={0}>
-                  <Col xs={12} sm={12} md={7} lg={7}>
-                    <Ludo />
-                  </Col>
-                  <Col xs={12} sm={12} md={5} lg={5}>
-                    <div className="sidebar">
+              <Row gutter={0}>
+                <Col xs={12} sm={12} md={7} lg={7}>
+                  <Ludo />
+                </Col>
+                <Col xs={12} sm={12} md={5} lg={5}>
+                  <div className="sidebar">
+                    <div>
                       <div>
-                        <div>
-                          <div className="desktop-header">
-                            <Header />
-                          </div>
-                          <Menu />
-                          <RestartGame />
-                          <Alert />
-                          <Dice />
-                          {activeWindow === "account" ? (
-                            <ControlWindowLayout
-                              toggle={() => setActiveWindow("")}
-                              title="PROFILE"
-                              subtitle="Your Profile Information"
-                            >
-                              <GameAccount />
-                            </ControlWindowLayout>
-                          ) : null}
-
-                          {activeWindow === "leaderboard" ? (
-                            <ControlWindowLayout
-                              toggle={() => setActiveWindow("")}
-                              title="LEADERBOARD"
-                              subtitle="Global Player Rankings"
-                            >
-                              <Leaderboard />
-                            </ControlWindowLayout>
-                          ) : null}
-
-                          {activeWindow === "multiplayer" ? (
-                            <ControlWindowLayout
-                              toggle={() => setActiveWindow("")}
-                              title="MULTIPLAYER"
-                              subtitle="Choose An Account To Play With"
-                            >
-                              <Multiplayer />
-                            </ControlWindowLayout>
-                          ) : null}
-
-                          {activeWindow === "toolbox" ? (
-                            <ControlWindowLayout
-                              toggle={() => setActiveWindow("")}
-                              title="TOOLBOX"
-                              subtitle="Get All Your Items And Settings Done"
-                            >
-                              <Toolbox
-                                activeCategory={activeCategory}
-                                onCategoryClick={handleCategoryClick}
-                              />
-                            </ControlWindowLayout>
-                          ) : null}
-
-                          {activeWindow === "help" ? (
-                            <ControlWindowLayout
-                              toggle={() => setActiveWindow("")}
-                              title="HELP"
-                              subtitle="Get Guides, Tips, And Tricks Needed For A Successful Game"
-                            >
-                              <GameHelp />
-                            </ControlWindowLayout>
-                          ) : null}
-                          <Control toggleActiveWindow={toggleActiveWindow} />
+                        <div className="desktop-header">
+                          <Header />
                         </div>
+                        <Menu />
+                        <Alert />
+                        {/* <RestartGame /> */}
+                        <Dice />
+                        {activeWindow === "account" ? (
+                          <ControlWindowLayout
+                            toggle={() => setActiveWindow("")}
+                            title="PROFILE"
+                            subtitle="Your Profile Information"
+                          >
+                            <GameAccount />
+                          </ControlWindowLayout>
+                        ) : null}
+
+                        {activeWindow === "leaderboard" ? (
+                          <ControlWindowLayout
+                            toggle={() => setActiveWindow("")}
+                            title="LEADERBOARD"
+                            subtitle="Global Player Rankings"
+                          >
+                            <Leaderboard />
+                          </ControlWindowLayout>
+                        ) : null}
+
+                        {activeWindow === "multiplayer" ? (
+                          <ControlWindowLayout
+                            toggle={() => setActiveWindow("")}
+                            title="MULTIPLAYER"
+                            subtitle="Choose An Account To Play With"
+                          >
+                            <Multiplayer />
+                          </ControlWindowLayout>
+                        ) : null}
+
+                        {activeWindow === "toolbox" ? (
+                          <ControlWindowLayout
+                            toggle={() => setActiveWindow("")}
+                            title="TOOLBOX"
+                            subtitle="Get All Your Items And Settings Done"
+                          >
+                            <Toolbox />
+                          </ControlWindowLayout>
+                        ) : null}
+
+                        {activeWindow === "help" ? (
+                          <ControlWindowLayout
+                            toggle={() => setActiveWindow("")}
+                            title="HELP"
+                            subtitle="Get Guides, Tips, And Tricks Needed For A Successful Game"
+                          >
+                            <GameHelp />
+                          </ControlWindowLayout>
+                        ) : null}
+                        <Control toggleActiveWindow={toggleActiveWindow} />
                       </div>
                     </div>
-                  </Col>
-                </Row>
-                <Footer />
-              </div>
-            </DiceProvider>
+                  </div>
+                </Col>
+              </Row>
+              <Footer />
+            </div>
           </BoardContext.Provider>
         </GameContext.Provider>
         <ToastContainer position="bottom-center" />
